862e52155c1d22f612bb2566722e8150eb54f93d
Rota911 <88843688+Rota911@users.noreply.github.com>
2024-10-15 16:59:42 +0200

    munka

--- f3/modifyCustomer.js
+++ f3/modifyCustomer.js
@@ -11,14 +11,39 @@ let customers = [
     {id:10, name:"Jonas Tungate", age:83, address:{city:"Padangulaktanding", street:"224 Manley Drive", house:49}, newsLetter: true}
 ]
 function isAddress(address) {    
-    //TODO
+    return (typeof(address.city) === 'string' && typeof(address.street) === 'string' && typeof(address.house) === 'number')
 }
 
 function isCustomer(name, age, address, newsLetter) {    
-    //TODO
+    return (
+        typeof(name) === "string" &&
+        typeof(age) === "number" &&
+        isAddress(address) &&
+        typeof(newsLetter) === "boolean"
+      )
 }
-function modifyCustomer (customer){
-    //TODO
-}
+function modifyCustomer(customer) {
+    if (!isCustomer(customer.name, customer.age, customer.address, customer.newsLetter)) return false;
+  
+    let copyCustomers = [...customers];
+    const index = copyCustomers.findIndex((cust) => cust.id === customer.id);
+  
+    if (index !== -1) {
+      const updatedCustomer = {
+        id: customer.id,
+        name: customer.name,
+        age: customer.age,
+        address: customer.address,
+        newsLetter: customer.newsLetter,
+      };
+  
+      copyCustomers[index] = updatedCustomer;
+  
+      return copyCustomers;
+    } else {
+      return false;
+    }
+  }
+  
 
 module.exports = modifyCustomer;
